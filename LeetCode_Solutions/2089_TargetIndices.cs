aa8d215b9665e8e6a5a21665c76d291795be73a7
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-07-27 20:23:19 -0400

    Added 2089. FindTarget and 912. SortArray
    2089. Find Target Indices After Sorting Array
    
    912. Sort an Array

---
+++ LeetCode_Solutions/2089_TargetIndices.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _2089_TargetIndices
+    {
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            // QuickSort(nums);
+            Array.Sort(nums);
+            IList<int> resultList = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    resultList.Add(i);
+                }
+            }
+            return resultList;
+        }
+
+        //public void QuickSort(int[] array)
+        //{
+        //    QuickSort(array, 0, array.Length - 1);
+        //}
+
+        //public void QuickSort(int[] array, int left, int right)
+        //{
+        //    if (left >= right)
+        //        return;
+
+        //    int i = left;
+        //    int j = right;
+
+        //    int pivot = array[(left + right) / 2];
+
+        //    while (i <= j)
+        //    {
+        //        while (array[i] < pivot)
+        //            i++;
+        //        while (array[j] > pivot)
+        //            j--;
+
+        //        if (i <= j)
+        //        {
+        //            int tmp = array[i];
+        //            array[i] = array[j];
+        //            array[j] = tmp;
+                
+        //            i++;
+        //            j--;
+        //        }
+
+        //        QuickSort(array, left, j);
+        //        QuickSort(array, i, right);
+        //    }
+
+        //}
+    }
+}
---
+++ LeetCode_Solutions/912_SortArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _912_SortArray
+    {
+        //912. Sort an Array
+        //Medium
+        
+        // Given an array of integers nums, sort the array in ascending order and return it.
+
+        // You must solve the problem without using any built-in functions in O(nlog(n)) 
+        // time complexity and with the smallest space complexity possible.
+
+        public int[] SortArray(int[] nums)
+        {
+            QuickSort(nums);
+            return nums;
+        }
+
+        public void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+        public void QuickSort(int[] array, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            int i = left;
+            int j = right;
+
+            int pivot = array[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                    i++;
+                while (array[j] > pivot)
+                    j--;
+
+                if (i <= j)
+                {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                    
+                    i++;
+                    j--;
+                }
+            }
+            QuickSort(array, i, right);
+            QuickSort(array, left, j);
+        }
+    }
+}
