46275303c58105ab8d7fa6a6b766e07088e686ca
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-09-05 10:05:39 -0400

    Added 27. Remove Element

---
+++ LeetCode_Solutions/26_RemoveDuplicates.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _26_RemoveDuplicates
+    {
+        /* 26. Remove Duplicates from Sorted Array
+        Given an integer array nums sorted in non-decreasing order, 
+        remove the duplicates in-place such that each unique element appears only once. 
+        The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+        Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+        Change the array nums such that the first k elements of nums contain the unique elements 
+        in the order they were present in nums initially. 
+        The remaining elements of nums are not important as well as the size of nums.
+
+        Return k.
+         */
+        public int RemoveDuplicates(int[] nums)
+        {
+            int k = -1;
+
+            var numsList = new List<int>();
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+                    if (nums[i] != nums[j])
+                    {
+                        numsList.Add(nums[i]);
+                    }
+                }
+            }
+
+            var newNums = numsList.ToArray();
+            k = newNums.Length;
+
+            return k;
+        }
+    }
+}
---
+++ LeetCode_Solutions/Remove_Element_27.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    /* 27. Remove Element
+     * 
+    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+    The order of the elements may be changed. 
+    Then return the number of elements in nums which are not equal to val.
+
+    Consider the number of elements in nums which are not equal to val be k, 
+    to get accepted, you need to do the following things:
+
+    Change the array nums such that the first k elements of nums 
+    contain the elements which are not equal to val. 
+    The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+     */
+    internal class Remove_Element_27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = nums.Length;
+            int countVal = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                if (nums[i] == val)
+                {
+                    countVal++;
+                }
+                else
+                {
+                    nums[i - countVal] = nums[i];
+                }
+            }
+
+            k -= countVal;
+            return k;
+        }
+    }
+}
