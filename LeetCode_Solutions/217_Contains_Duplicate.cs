5c429d7325310273c119554cf1fb888e0550899a
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-10-14 17:42:36 -0400

    Added 217. Contains Duplicate

---
+++ LeetCode_Solutions/217_Contains_Duplicate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    /*  217. Contains Duplicate
+
+        Given an integer array nums, return true 
+        if any value appears at least twice in the array, 
+        and return false if every element is distinct.
+     */
+    internal class _217_Contains_Duplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> uniqueNums = new HashSet<int>();
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                uniqueNums.Add(nums[i]);
+            }
+
+            if(uniqueNums.Count == nums.Length)
+                return false;
+
+            return true;
+        }
+    }
+}
