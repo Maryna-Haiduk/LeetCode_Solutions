79ba46788c634a5f459e2fe54b6e395bf375d165
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-10-16 20:20:31 -0400

    Added 290. Word Pattern

---
+++ LeetCode_Solutions/202_Happy_Number.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _202_Happy_Number
+    {
+        /*202. Happy Number
+        Write an algorithm to determine if a number n is happy.
+
+        A happy number is a number defined by the following process:
+
+        Starting with any positive integer, replace the number by the sum of the squares of its digits.
+        Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+        Those numbers for which this process ends in 1 are happy.
+        Return true if n is a happy number, and false if not.
+                Example 1:
+
+        Input: n = 19
+        Output: true
+        Explanation:
+        12 + 92 = 82
+        82 + 22 = 68
+        62 + 82 = 100
+        12 + 02 + 02 = 1
+        */
+
+        public bool IsHappy(int n)
+        {
+            int[] digits = n.ToString().ToCharArray().Select(Convert.ToInt32).ToArray();
+
+            if(digits.Length < 1)
+            {
+                return false;
+            }
+
+            int tmp = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                digits[i] += tmp;
+            }
+
+            tmp *= digits.Length;
+
+            int result = 0;
+
+            while (result == 1)
+            {
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode_Solutions/290_Word_Pattern.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    /* 290. Word Pattern
+     * 
+     * Given a pattern and a string s, find if s follows the same pattern.
+     * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+     * 
+     * Examples:
+
+        Input: pattern = "abba", s = "dog cat cat dog"
+        Output: true
+
+        Input: pattern = "abba", s = "dog cat cat fish"
+        Output: false
+    */
+    internal class _290_Word_Pattern
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            char[] patternKeys = pattern.ToCharArray();
+            string[] sValues = s.Split(" ");
+
+            if (patternKeys.Length != sValues.Length)
+            {
+                return false; // The pattern and string length mismatch.
+            }
+
+            Dictionary<char, string> patternToWord = new Dictionary<char, string>();
+            Dictionary<string, char> wordToPattern = new Dictionary<string, char>();
+
+            for (int i = 0; i < patternKeys.Length; i++)
+            {
+                char currentChar = patternKeys[i];
+                string currentString = sValues[i];
+
+                if (patternToWord.ContainsKey(currentChar) && patternToWord[currentChar] != currentString)
+                {
+                    return false;
+                }
+                else
+                {
+                    patternToWord[currentChar] = currentString;
+                }
+
+                if (wordToPattern.ContainsKey(currentString) && wordToPattern[currentString] != currentChar)
+                {
+                    return false;
+                }
+                else
+                {
+                    wordToPattern[currentString] = currentChar;
+                }
+            }
+           
+            
+            return true;
+        }
+    }
+}
