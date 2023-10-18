c7e621a9e0246cc8625125c768c2eca92fce692c
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-10-17 20:23:46 -0400

    Added 387. First Unique Character in a String

---
+++ LeetCode_Solutions/387_First_Unique_Character_in_a_String.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _387_First_Unique_Character_in_a_String
+    {
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (charCount.ContainsKey(c))
+                    charCount[c]++;
+                else
+                    charCount[c] = 1;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i]] == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
