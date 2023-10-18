ab174c771ef7e566e55aa496cebfa9d5dda7e81e
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-10-17 22:45:09 -0400

    Added 58. Length of Last Word

---
+++ LeetCode_Solutions/58_Length_of_Last_Word.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _58_Length_of_Last_Word
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int lastWordLength = 0;
+            
+            string[] array = s.Split(" ");
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (string.IsNullOrEmpty(array[i]))
+                { 
+                    continue; 
+                }
+                else
+                {
+                    return lastWordLength = array[i].Length;
+                }
+            }
+            return lastWordLength;
+        }
+
+        public int LengthOfLastWordSecondSolution(string s)
+        {
+            int lastWordLength = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    if (lastWordLength == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return lastWordLength;
+                    }
+                }
+                lastWordLength++;
+            }
+            return lastWordLength;
+        }
+    }
+}
