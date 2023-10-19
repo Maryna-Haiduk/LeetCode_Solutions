d43de3d2b5c801e197273e84d057a19780568c9f
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-10-18 22:09:33 -0400

    Added 383. Ransom Note

---
+++ LeetCode_Solutions/383_Ransom_Note.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _383_Ransom_Note
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+
+            Dictionary<char, int> magazineDictionary = new Dictionary<char, int>();
+
+            foreach (char c in magazine)
+            {
+                if(magazineDictionary.ContainsKey(c))
+                {
+                    magazineDictionary[c]++;
+                }
+                else
+                {
+                    magazineDictionary.Add(c, 1);
+                }
+            }
+
+
+            foreach (char c in ransomNote)
+            {
+                if (magazineDictionary.ContainsKey(c) && magazineDictionary[c] > 0)
+                {
+                    magazineDictionary[c]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
