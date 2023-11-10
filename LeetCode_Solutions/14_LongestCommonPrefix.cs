409ab99b1bbb1defc0bc87a1cd90fabacebcb03b
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-11-09 21:48:31 -0500

    Added 14_LongestCommonPrefix

---
+++ LeetCode_Solutions/14_LongestCommonPrefix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _14_LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0 || string.IsNullOrEmpty(strs[0]))
+            {
+                return "";
+            }
+
+            List<char> prefixList = new List<char>();
+
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                char current = strs[0][i];
+
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length || strs[j][i] != current)
+                    {
+                        return new string(prefixList.ToArray());
+                    }
+                    if (j == strs.Length - 1)
+                    {
+                        prefixList.Add(current);
+                    }
+                }
+            }
+
+            string commonPrefix = new string(prefixList.ToArray());
+
+            if (commonPrefix.Length > 0)
+                return commonPrefix;
+
+            return "";
+        }
+    }
+}
