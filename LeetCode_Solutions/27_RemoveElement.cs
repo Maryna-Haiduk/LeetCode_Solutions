ab72f30af87045ee812dfbcce3b4b5f34c1c5cad
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-11-07 12:35:59 -0500

    Added RemoveElement_27

---
+++ LeetCode_Solutions/27_RemoveElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class RemoveElement_27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            int[] newNums = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    newNums[k] = nums[i];
+                    k++;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = newNums[i];
+            }
+            return k;
+        }
+    }
+}
---
+++ LeetCode_Solutions/_501_FindMode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _501_FindModeBST
+    {
+        public int[] FindMode(TreeNode root)
+        {
+            var modes = new List<int>();
+
+
+            var result = modes.ToArray();
+            return result;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right=null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+    }
+}
